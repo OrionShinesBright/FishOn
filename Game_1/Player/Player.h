2c526ee59865b876d89b38798604e1e5e78df59b
Haider Ali <haideralijafri2007@gmail.com>
2025-06-16 11:46:47 +0500

    Add Player class files

--- Game_1/Player/Player.h
+++ Game_1/Player/Player.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Player
+{
+private:
+	string name;
+	int score;
+	int gamesWon;
+
+public:
+	void updateScore(int points);
+	void incrementWins();
+	void displayStats();
+};
 
