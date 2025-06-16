bb5321e9bae6f839e18786b665fc8456bae3a8cc
Haider Ali <haideralijafri2007@gmail.com>
2025-06-16 11:46:08 +0500

    Add GameManager class files

---
+++ Game_1/GameManager/GameManager.cpp
@@ -0,0 +1,18 @@
+#include "GameManager.h"
+
+void GameManager::startGame()
+{
+}
+
+void GameManager::processMove()
+{
+}
+
+void GameManager::switchPlayer()
+{
+}
+
+bool GameManager::isGameOver()
+{
+	return false;
+}
--- Game_1/GameManager/GameManager.h
+++ Game_1/GameManager/GameManager.h
@@ -1,1 +1,18 @@
+#pragma once
+#include "GameBoard/GameBoard.h"
+#include "Player/Player.h"
+
+class GameManager
+{
+private:
+	GameBoard board;
+	Player players[2];
+	int currentPlayer;
+
+public:
+	void startGame();
+	void processMove();
+	void switchPlayer();
+	bool isGameOver();
+};
 
