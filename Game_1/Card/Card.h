04b9f7a9e6e6440f32f836621f83206e4accb7fd
OrionShinesBright <rafay880333@gmail.com>
2025-06-16 13:57:28 +0500

    Nothing important

--- Game_1/Card/Card.h
+++ Game_1/Card/Card.h
@@ -8,6 +8,15 @@ private:
 
 public:
 	void flip();
+
 	bool checkMatch(Card other);
 	void draw();
+
+	inline void setValue(int val) { value = val; }
+	inline void setIsFlipped(bool isFlip) { isFlipped = isFlip; }
+	inline void setIsMatched(bool isMat) { isMatched = isMat; }
+
+	inline int getValue() const { return value; }
+	inline bool getIsFlipped() const { return isFlipped; }
+	inline bool getIsMatched() const { return isMatched; }
 };
