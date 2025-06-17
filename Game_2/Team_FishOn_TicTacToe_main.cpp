0845a04c5828da4067c85f72dc0d58a344cc6156
OrionShinesBright <rafay880333@gmail.com>
2025-06-17 17:25:56 +0500

    Update Team_FishOn_TicTacToe_main.cpp

--- Game_2/Team_FishOn_TicTacToe_main.cpp
+++ Game_2/Team_FishOn_TicTacToe_main.cpp
@@ -8,7 +8,14 @@ int main() {
  	GAME LOOP:
   	Now.. listen to me.. idk what condition is required to call the AI move, but, 
    	whenever that conditin is satisfied, this is how you must call the AI's move:
-    		sf::Vector2i move = aiPlayer.getBestMove(board, currentSymbol);
+    		char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE]; //cuz its a square DUH
+		// Fill in board with current game state
+		AIPlayer ai('O', 'X');
+		sf::Vector2i move = ai.getBestMove(board, currentBoardSize);
+  	Also, don't worry about whether you need to call the easy medium or hard AI,
+   	cuz I have already made this function check what szed board we have.. this is important
+    	cuz smol board == weak ai.. so.. you dont need to handle that here. it is already
+     	handled in the class itself.
   	*/
 	
 	return 0;
