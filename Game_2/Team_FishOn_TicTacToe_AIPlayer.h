bc3ef0133bc898824dba4deef4ca4571f90242a3
OrionShinesBright <rafay880333@gmail.com>
2025-06-17 17:20:58 +0500

    Update Team_FishOn_TicTacToe_AIPlayer.h

--- Game_2/Team_FishOn_TicTacToe_AIPlayer.h
+++ Game_2/Team_FishOn_TicTacToe_AIPlayer.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <SFML/System.hpp>
 
+class AIPlayer {
+public:
+    AIPlayer(char aiSymbol, char playerSymbol);
+    sf::Vector2i getBestMove(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size);
+
+private:
+    char aiSymbol, playerSymbol;
+    int boardSize;
+
+    bool isWinningMove(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int row, int col, char symbol);
+    int evaluatePosition(int row, int col);
+};
