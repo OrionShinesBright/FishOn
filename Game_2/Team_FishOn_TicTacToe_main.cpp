f993b2a6e4301b4536238109d190d0e593956c92
OrionShinesBright <rafay880333@gmail.com>
2025-06-17 17:12:56 +0500

    Update Team_FishOn_TicTacToe_main.cpp

--- Game_2/Team_FishOn_TicTacToe_main.cpp
+++ Game_2/Team_FishOn_TicTacToe_main.cpp
@@ -2,5 +2,13 @@
 
 int main() {
 	std::cout << "Hello, World!\n";
+
+	/*
+ 	GAME LOOP:
+  	Now.. listen to me.. idk what condition is required to call the AI move, but, 
+   	whenever that conditin is satisfied, this is how you must call the AI's move:
+    		sf::Vector2i move = aiPlayer.getBestMove(board, currentSymbol);
+  	*/
+	
 	return 0;
 }
