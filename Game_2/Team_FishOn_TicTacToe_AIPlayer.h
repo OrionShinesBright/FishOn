c21fb5fcd350785428835fbd7370757e5803d78f
OrionShinesBright <rafay880333@gmail.com>
2025-06-17 17:31:31 +0500

    Update Team_FishOn_TicTacToe_AIPlayer.h

--- Game_2/Team_FishOn_TicTacToe_AIPlayer.h
+++ Game_2/Team_FishOn_TicTacToe_AIPlayer.h
@@ -2,14 +2,16 @@
 #include <SFML/System.hpp>
 
 class AIPlayer {
-public:
-    AIPlayer(char aiSymbol, char playerSymbol);
-    sf::Vector2i getBestMove(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size);
-
 private:
     char aiSymbol, playerSymbol;
     int boardSize;
-
+    //I know what you're thinking.. yes.. these are private funcs.. stop nosing around..
+    //functions have the right to privaxcy too
     bool isWinningMove(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int row, int col, char symbol);
     int evaluatePosition(int row, int col);
+
+public:
+    AIPlayer(char aiSymbol, char playerSymbol);
+    //this function is beyond me.. but it was necessary.. i had to do it
+    sf::Vector2i getBestMove(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size);
 };
